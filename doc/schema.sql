e63df33fe13e7f4ff64c663840aaffe70ecf8280
Ben Lu <ocblue19446@gmail.com>
2022-09-24 16:22:22 -0400

    db schema 1st cut

--- doc/schema.sql
+++ doc/schema.sql
@@ -1,15 +1,16 @@
 -- SQL dump generated using DBML (dbml-lang.org)
 -- Database: PostgreSQL
--- Generated at: 2022-09-24T15:46:30.128Z
+-- Generated at: 2022-09-24T20:21:09.985Z
 
 CREATE TABLE "users" (
   "username" varchar PRIMARY KEY,
   "hashed_password" varchar NOT NULL,
   "full_name" varchar NOT NULL,
   "email" varchar UNIQUE NOT NULL,
+  "address_id" bigint,
   "agency" varchar,
-  "app_contact_name" varchar,
-  "app_contact_email" varchar,
+  "contact_name" varchar,
+  "contact_email" varchar,
   "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01',
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
@@ -18,6 +19,7 @@ CREATE TABLE "clients" (
   "id" bigserial PRIMARY KEY,
   "agent" varchar NOT NULL,
   "full_name" varchar NOT NULL,
+  "address_id" bigint,
   "birth_date" timestamptz NOT NULL,
   "driver_license_number" varchar,
   "driver_license_state" varchar,
@@ -27,7 +29,6 @@ CREATE TABLE "clients" (
 
 CREATE TABLE "addresses" (
   "id" bigserial PRIMARY KEY,
-  "client_id" bigint NOT NULL,
   "address_line1" varchar NOT NULL,
   "address_line2" varchar,
   "city" varchar NOT NULL,
@@ -40,10 +41,12 @@ CREATE TABLE "addresses" (
 CREATE TABLE "applications" (
   "id" bigserial PRIMARY KEY,
   "agent" varchar NOT NULL,
+  "is_formal" boolean NOT NULL,
+  "is_1035_exchange" boolean,
   "primary_insured_id" bigint NOT NULL,
   "joint_insured_id" bigint,
   "carriers" varchar NOT NULL,
-  "product_type_generic" varchar NOT NULL,
+  "product_type" varchar NOT NULL,
   "applied_amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
@@ -52,15 +55,13 @@ CREATE TABLE "documents" (
   "id" bigserial PRIMARY KEY,
   "application_id" bigint NOT NULL,
   "client_id" bigint NOT NULL,
-  "type" varchar NOT NULL,
+  "doc_type" varchar NOT NULL,
+  "file_name" varchar NOT NULL,
   "file_size" varchar NOT NULL,
+  "file_type" varchar NOT NULL,
   "s3_url" varchar NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
-
-CREATE INDEX ON "users" ("full_name");
-
-CREATE INDEX ON "users" ("email");
 
 CREATE INDEX ON "clients" ("agent");
 
@@ -70,11 +71,9 @@ CREATE INDEX ON "clients" ("agent", "birth_date");
 
 CREATE INDEX ON "clients" ("agent", "driver_license_number");
 
-CREATE INDEX ON "addresses" ("client_id");
-
 CREATE INDEX ON "addresses" ("address_line1");
 
-CREATE INDEX ON "addresses" ("state");
+CREATE INDEX ON "addresses" ("zip_code");
 
 CREATE INDEX ON "applications" ("agent");
 
@@ -84,15 +83,17 @@ CREATE INDEX ON "applications" ("agent", "joint_insured_id");
 
 CREATE INDEX ON "documents" ("application_id");
 
-CREATE INDEX ON "documents" ("application_id", "client_id");
+CREATE INDEX ON "documents" ("application_id", "file_name");
 
-COMMENT ON COLUMN "applications"."product_type_generic" IS 'non carrier-specific product type';
+COMMENT ON COLUMN "applications"."product_type" IS 'non carrier-specific product type';
 
 COMMENT ON COLUMN "documents"."s3_url" IS 's3 bucket url';
 
+ALTER TABLE "users" ADD FOREIGN KEY ("address_id") REFERENCES "addresses" ("id");
+
 ALTER TABLE "clients" ADD FOREIGN KEY ("agent") REFERENCES "users" ("username");
 
-ALTER TABLE "addresses" ADD FOREIGN KEY ("client_id") REFERENCES "clients" ("id");
+ALTER TABLE "clients" ADD FOREIGN KEY ("address_id") REFERENCES "addresses" ("id");
 
 ALTER TABLE "applications" ADD FOREIGN KEY ("agent") REFERENCES "users" ("username");
 
